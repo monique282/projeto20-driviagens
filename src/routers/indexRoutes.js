66d53deceecea37f8c5819dc45b26f6951dea020
monique <moniquejp16@gmail.com>
2023-09-05 05:01:00 -0300

    criando as rotas, indexRoutes

---
+++ src/routers/indexRoutes.js
@@ -0,0 +1,13 @@
+// esse arquivo serve pra unir todos que eu estou escrevendo Rotas que esta dentro de Routes
+// lebrando que todas as Rotas aqui vai pro app
+
+import { Router } from "express";
+
+
+const router = Router()
+
+router.use([
+
+]);
+
+export default router;
