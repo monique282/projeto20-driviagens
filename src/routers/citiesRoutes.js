64f8a109cef10376c5faf112c6ec31091e71d52b
monique <moniquejp16@gmail.com>
2023-09-07 05:14:20 -0300

    fix: atializando a função chamada na rota, citesRoutes

--- src/routers/citiesRoutes.js
+++ src/routers/citiesRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { validateJoiForAll } from "../middlewares/validateSchema.js";
 import { citiesTable } from "../schemas/ciFliPaTrSchema.js";
+import { citesPost } from "../controllers/citesControllers.js";
 
 const citiesRouter = Router();
 
-citiesRouter.post("/cities", validateJoiForAll(citiesTable), citesControlles);
+citiesRouter.post("/cities", validateJoiForAll(citiesTable), citesPost);
 
 export default citiesRouter;
