03d7149924397b699a9c1f946afeb7894962a95c
monique <moniquejp16@gmail.com>
2023-09-07 05:17:29 -0300

    feat: criando a função sobre a rota de cidades, citesServises

---
+++ src/services/citesServices.js
@@ -0,0 +1,12 @@
+
+// essa função serve para poder tratas os assuntos de serviços
+// ela é chamada nos controles
+// ela chama o repository
+
+ async function citesPost(name){
+
+    const result = await citesRepositoy.citesPost (name);
+    return result;
+ }
+
+ export const firstNameLastServices = {citesPost}
