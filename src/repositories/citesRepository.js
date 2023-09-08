08b29b239bd2d42fa177b7ec23c87c3aa0bdf699
monique <moniquejp16@gmail.com>
2023-09-08 04:23:38 -0300

    feat: adicionando a requisição para pegar a cidade pelo nome, citesRepository, citesNameGet

--- src/repositories/citesRepository.js
+++ src/repositories/citesRepository.js
@@ -3,10 +3,20 @@ import { db } from "../dataBase/databaseConnection.js";
 // função que envia para o banco o nome da cidade
 // ela pe chamada la no sevice
 
+// essa função e pra adicionar a cidades no banco
 async function citesPost(name) {
 
     const serveSend = await db.query('INSERT INTO cities (name) VALUES ($1)', [name]);
     return serveSend;
 };
 
-export const citesRepositoy = { citesPost }
+// essa função é pra pegar toas as cidade ja aciodopnadas no banco
+
+async function citesGetName(name){
+
+    const citesNameResult = await db.query('SELECT * FROM cities WHERE name = $1;' [name]);
+    return citesNameResult;
+}
+
+
+export const citesRepositoy = { citesPost , citesGetName}
