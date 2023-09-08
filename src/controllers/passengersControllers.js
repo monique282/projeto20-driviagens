d71a93e89035144016f3a60e2a6de5404ea6869b
monique <moniquejp16@gmail.com>
2023-09-08 03:40:19 -0300

    fix: retirando o try/catch , passengersControllers

--- src/controllers/passengersControllers.js
+++ src/controllers/passengersControllers.js
@@ -7,16 +7,10 @@ export async function firstNameLastNamePost(req, res) {
 
     // pegando os dados enviados pelo body
     const { firstName, lastName } = req.body;
-    console.log(firstName)
-    console.log(lastName)
 
-    try {
         // fazendo a requisição enviar o nome e sobrenome para o banco
         const result = await firstNameLastServices.firstNameLastNamePost(firstName, lastName);
         // se tudo der certo
         res.sendStatus(httpStatus.NO_CONTENT);
 
-    } catch (erro) {
-        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    };
 }
