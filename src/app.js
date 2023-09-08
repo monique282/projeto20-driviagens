7660271fcf7b0813b055274c8a064f02964f7831
monique <moniquejp16@gmail.com>
2023-09-08 03:37:08 -0300

    feat: importando e colocando o app.use sobre o errorHaldler, app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Router, json } from "express";
 import dotenv from "dotenv";
 import router from "./routers/indexRoutes.js";
+import errorHandler from "./middlewares/errorMiddlewares.js";
 
 //configuração da API
 dotenv.config();
@@ -11,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(json());
 app.use(router);
-app.use(erro)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
