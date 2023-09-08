5be581ede124d4290d22dccd8ed91b07e109d922
monique <moniquejp16@gmail.com>
2023-09-08 03:39:21 -0300

    feat: importando e colocando o app.use sobre o errorHaldler, app

--- src/controllers/citesControllers.js
+++ src/controllers/citesControllers.js
@@ -11,13 +11,8 @@ export async function citesPost(req, res) {
     // pegando os dados enviados pelo body
     const { name } = req.body;
 
-    try {
         // fazendo a requisição enviar o nome da cidade para o banco
         const result = await citesServices.citesPost (name);
         // se tudo der certo
         res.sendStatus(httpStatus.NO_CONTENT);
-
-    } catch (erro) {
-        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    };
 }
