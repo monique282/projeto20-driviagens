fc7838b303c630f0a11ce9c86a25f593ef329e78
monique <moniquejp16@gmail.com>
2023-09-10 14:37:28 -0300

    feat: pegando a reserva do voo pelo id, idflightsGet, flightsRepository

--- src/repositories/flightsRepository.js
+++ src/repositories/flightsRepository.js
@@ -6,5 +6,12 @@ import { db } from "../dataBase/databaseConnection.js";
       const serveSend = await db.query('INSERT INTO flights ( origin, destination, date) VALUES ($1, $2, $3)', [origin, destination, date]);
       return serveSend;
   };
+
+  //função pra pegar o voo por id
+async function idflightsGet(id) {
+
+    const serveSend = await db.query('SELECT * FROM flights WHERE id = $1;', [id]);
+    return serveSend;
+};
   
- export const flightsRepository = {flightsPost}
+ export const flightsRepository = {flightsPost, idflightsGet}
