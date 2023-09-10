c824eb997b27be5e14a4979bd8bd5c92b384c8b9
monique <moniquejp16@gmail.com>
2023-09-10 10:55:36 -0300

    fix, atualizando a validação do joi, flightsRoutes

--- src/routers/flightsRoutes.js
+++ src/routers/flightsRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateJoiForAll } from "../middlewares/validateSchema.js";
+import { flightsTable } from "../schemas/ciFliPaTrSchema.js";
 
 
 const flightsRouter = Router();
 
-flightsRouter.post("/flights", validateJoiForAll());
+flightsRouter.post("/flights", validateJoiForAll(flightsTable));
 flightsRouter.get("/flights");
 
 export default flightsRouter;
