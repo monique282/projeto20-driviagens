aef890d734c06c064d509cfeb4c2960b6b3eb612
monique <moniquejp16@gmail.com>
2023-09-11 04:09:20 -0300

    fix: passengersServices

--- src/services/passengersServices.js
+++ src/services/passengersServices.js
@@ -1,24 +1,35 @@
+// essa pasta serve para poder tratar os assuntos de serviços
+// ela é chamada nos controles
+// ela chama o repository
+
 import { errors } from "../errors/allMistakes.js";
 import { passengersRepository } from "../repositories/passengersRepository.js";
 
 async function passengersPost(firstName, lastName) {
 
+   //verificando de os nomes e nobrenome não é valido
+   // nome não valido
    if (!firstName || firstName === "") {
-      throw errors.notFound("Nome")
-   }
+      throw errors.notFound("Nome");
+   };
+
+   // sobrenome não valido
    if (!lastName || lastName === "") {
-      throw errors.notFound("Sobre-nome")
-   }
+      throw errors.notFound("Sobre-nome");
+   };
 
-
+// se tufo der certo enviar os dados pro banco
    const result = await passengersRepository.passengersPost(firstName, lastName);
    return result;
-}
+};
 
+// função para pegar os dados dos passageiros
 async function passengersGet(name) {
 
+   // não tem nenhuma validação
+   // ja que tudo deu certo
    const result = await passengersRepository.passengersGet(name);
    return result;
 }
 
-export const passengersServices = { passengersPost, passengersGet }
+export const passengersServices = { passengersPost, passengersGet };
