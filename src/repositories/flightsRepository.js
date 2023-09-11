e6eaddb45b6448a2994904eea86253ba2354d584
monique <moniquejp16@gmail.com>
2023-09-11 03:46:26 -0300

    fix: flightsRepository

--- src/repositories/flightsRepository.js
+++ src/repositories/flightsRepository.js
@@ -2,14 +2,14 @@
 
 import { db } from "../dataBase/databaseConnection.js";
 
-
 // função que envia para o banco o nome e o sobre nome do passageiro
 async function flightsPost(origin, destination, date) {
+
     const serveSend = await db.query('INSERT INTO flights ( origin, destination, date) VALUES ($1, $2, $3)', [origin, destination, date]);
     return serveSend;
 };
 
-//função pra pegar o voo por id
+//função pra pegar o voo buscando pelo id
 async function idflightsGet(id) {
 
     const serveSend = await db.query('SELECT * FROM flights WHERE id = $1;', [id]);
