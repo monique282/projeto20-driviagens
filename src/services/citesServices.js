6bf831932adb144d49738ffb044e0f2040166569
monique <moniquejp16@gmail.com>
2023-09-11 03:58:42 -0300

    fix: cites.Services

--- src/services/citesServices.js
+++ src/services/citesServices.js
@@ -6,14 +6,18 @@
 import { errors } from "../errors/allMistakes.js";
 import { citesRepository } from "../repositories/citesRepository.js";
 
+// função para verificas ja existe uma cidade com aquele nome cadastrado
 async function citesPost(name) {
    const thereIsCity = await citesRepository.citesNameGet(name);
+
+   // pegando a requicição e vendo se ja existe a cidade
    if(thereIsCity.length !== 0){
       throw errors.conflict(name);
    }
 
+   // se tudo der certo
     const result = await citesRepository.citesPost(name);
    return result;
-}
+};
 
-export const citesServices = { citesPost }
+export const citesServices = { citesPost };
