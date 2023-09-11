94b74f4a5f1e65a902c14dd60a53543c2c942f27
monique <moniquejp16@gmail.com>
2023-09-11 03:44:53 -0300

    fix: citesRepository

--- src/repositories/citesRepository.js
+++ src/repositories/citesRepository.js
@@ -1,28 +1,27 @@
-// essa função é chamada la la no sevises
-// função que envia para o banco o nome da cidade
+// essa função é chamada la no sevises
 
 import { db } from "../dataBase/databaseConnection.js";
 
-// essa função e pra adicionar a cidades no banco
+// essa função e pra adicionar a cidade no banco
 async function citesPost(name) {
     
     const serveSend = await db.query('INSERT INTO cities (name) VALUES ($1);', [name]);
     return serveSend;
 };
 
-// essa função é pra pegar toas as cidade pelo nome 
+// essa função é pra pegar todas as cidade buscando pelo nome 
 async function citesNameGet(name){
 
     const citesNameResult = await db.query('SELECT * FROM cities WHERE name = $1;', [name]);
     return citesNameResult.rows;
-}
+};
 
-// essa função é pra pegar a cidade pelo id
+// essa função é pra pegar a cidade buscando pelo id
 async function citesIdGet(id){
 
     const citesNameResult = await db.query('SELECT * FROM cities WHERE id = $1;', [id]);
     return citesNameResult.rows;
-}
+};
 
 
-export const citesRepository = { citesPost , citesNameGet, citesIdGet}
+export const citesRepository = { citesPost , citesNameGet, citesIdGet};
