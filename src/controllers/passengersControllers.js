680a9992b0aa901518f210076d3b0bc4abd90ffd
monique <moniquejp16@gmail.com>
2023-09-11 03:37:06 -0300

    fix: passengersControllers

--- src/controllers/passengersControllers.js
+++ src/controllers/passengersControllers.js
@@ -10,12 +10,11 @@ export async function passengersPost(req, res) {
     // pegando os dados enviados pelo body
     const { firstName, lastName } = req.body;
 
-    // fazendo a requisição enviar o nome e sobrenome para o banco
+    // fazendo a requisição verificar se ta tudo certo
     const result = await passengersServices.passengersPost(firstName, lastName);
    
     // se tudo der certo
     res.sendStatus(httpStatus.OK);
-
 }
 
 // função que pega os dados de nome e quantidade de vools
@@ -24,7 +23,7 @@ export async function passengersGet(req, res) {
     // pegando os dados enviados pelo query
     const { name } = req.query;
 
-    // fazendo a requisição enviar o nome e sobrenome para o banco
+    // fazendo a requisição epara ver se ta tudo certo
     const result = await passengersServices.passengersGet(name);
    
     // se tudo der certo
