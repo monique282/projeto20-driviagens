8e485e3ad95e7dd1902cf5fc2efc0b426936a767
monique <moniquejp16@gmail.com>
2023-09-11 03:40:00 -0300

    fix: flightsControllers

--- src/controllers/flightsControllers.js
+++ src/controllers/flightsControllers.js
@@ -4,7 +4,7 @@
 import httpStatus from "http-status";
 import { flightsServices } from "../services/flightsServices.js";
 
-// função que pega os dados de da viagem como destino partida e data de voo
+// função que envia os dados da viagem como origem ,destino e data de voo
 export async function flightsPost(req, res) {
 
     // pegando os dados enviados pelo body
@@ -20,11 +20,13 @@ export async function flightsPost(req, res) {
 // função que pega os dados de da viagem como destino partida e data de voo
 export async function flightsGet(req, res) {
 
-    const origin = req.query.origin
-    const destination = req.query.destination
-    const smallerDate = req.query['smaller-date'];
-    const biggerDate = req.query['bigger-date'];
-    
+    // dados enviados pela query
+    const {
+        origin,
+        destination,
+        'smaller-date': smallerDate,
+        'bigger-date': biggerDate } = req.query;
+
     // fazendo a verificação se ta tudo certo
     const result = await flightsServices.flightsGet(origin, destination, smallerDate, biggerDate);
 
