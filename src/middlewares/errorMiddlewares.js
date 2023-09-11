1c5d5a3ae52c500dcb401c377f4f937570126e58
monique <moniquejp16@gmail.com>
2023-09-11 04:15:07 -0300

    fix: errorMiddlewares

--- src/middlewares/errorMiddlewares.js
+++ src/middlewares/errorMiddlewares.js
@@ -7,27 +7,27 @@ export default function errorHandler (error, req, res, next){
 // erro de conflito 409
 if(error.type === "conflict"){
     return res.status(httpStatus.CONFLICT).send(error.message);
-}
+};
 
 // erro de "não encontrado" 404
 if(error.type === "notFound"){
-    return res.status(httpStatus.NOT_FOUND).send(error.message)
-}
+    return res.status(httpStatus.NOT_FOUND).send(error.message);
+};
 
 // erro de "entidade nao processavel" 422
 if(error.type === "Unprocessable Entity"){
-    return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.message)
-}
+    return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.message);
+};
 
 // erro de "pedido ruis" 400
 if(error.type === "Bad request"){
-    return res.status(httpStatus.BAD_REQUEST).send(error.message)
-}
+    return res.status(httpStatus.BAD_REQUEST).send(error.message);
+};
 
 // erro de "erro do sevidor interno" 500
 if(error.type === "internal server error"){
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message)
-}
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+};
 
-return res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Sorry, something went wrong")
-}
+return res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Sorry, something went wrong");
+};
