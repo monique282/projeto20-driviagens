165b1a6e5ffa7c06600a7dbef63719887a18a56a
monique <moniquejp16@gmail.com>
2023-09-11 04:05:56 -0300

    fix: flightsServices

--- src/services/flightsServices.js
+++ src/services/flightsServices.js
@@ -11,25 +11,30 @@ import { flightsRepository } from "../repositories/flightsRepository.js";
 import { flightsGetTable } from "../schemas/ciFliPaTrSchema.js";
 dayjs.extend(customParseFormat);
 
+//função para enviar pro banco os dados de origem destino e data
 async function flightsPost(origin, destination, date) {
 
     // verificando se a cidade de origin existe na tabela de cidades
     const thereIsCityOrigin = await citesRepository.citesIdGet(origin);
+
+    // se existe
     if (thereIsCityOrigin.length === 0) {
         throw errors.notFound("A cidade");
-    }
+    };
 
     // verificando se a cidade de destino existe na tabela de cidades
     const thereIsCityDestination = await citesRepository.citesIdGet(destination);
+
+    // se existe
     if (thereIsCityDestination.length === 0) {
         throw errors.notFound("A cidade ");
-    }
+    };
 
-    //verificando de origem e destino são iguais
+    //verificando se origem e destino são iguais
     if (origin === destination) {
         // se forem iguais tem que dar conflito
         throw errors.equalConflicts();
-    }
+    };
 
     // preciso verificar se a data do voo é no futuro
     // pegando a data atual
@@ -39,56 +44,62 @@ async function flightsPost(origin, destination, date) {
     const flightDate = dayjs(date, 'DD-MM-YYYY');
 
     // verificando de a data do voo é anterior a data atual
+    // se for
     if (flightDate.isBefore(currentDate)) {
         // se a data do voo não for maior que a data atual
         throw errors.UnprocessableEntity("A data do voo deve ser maior do que a data atual.");
-    }
+    };
 
     //se tudo cer certo vou enviar os dados para o baco
     const result = await flightsRepository.flightsPost(origin, destination, date);
     return result;
+};
 
-}
-
+//função para pegar as informações de nome e sobreno de usuario junto com a quantidade que cada usuario de de voos registrados
 async function flightsGet(origin, destination, smallerDate, biggerDate) {
 
-    const { error: errorDateSmallerDate } = flightsGetTable.validate(smallerDate)
-    const { error: errorDateBiggerDate } = flightsGetTable.validate(biggerDate)
+    //validando se as datas estão no formato esperado
+    const { error: errorDateSmallerDate } = flightsGetTable.validate(smallerDate);
+    const { error: errorDateBiggerDate } = flightsGetTable.validate(biggerDate);
 
-    // verificando de o o formato das datas estão ok
+    // verificando se o formato das datas estão ok
+    // não esta
     if (errorDateSmallerDate || errorDateBiggerDate) {
         throw UnprocessableEntity("Formado de data invalida")
-    }
+    };
 
     // verificar de a data smallerDate > biggerDate
-
-    // transformando a informação da data passada smallerDate pelo usuario
+    // transformando a informação pelo usuario smallerDate, para uma data
     const currentDate = dayjs(smallerDate, 'DD-MM-YYYY');
 
-    // transformando a informação da data passada biggerDate pelo usuario
+    // transformando a informação pelo usuario biggerDate, para uma data
     const flightDate = dayjs(biggerDate, 'DD-MM-YYYY');
 
-    // verificando de a data do voo é anterior a data atual
+    // verificando se a data do voo é anterior a data atual
+    // se é
     if (flightDate.isBefore(currentDate)) {
-        // se a data do voo não for maior que a data atual
+
+        // se a data do voo for no passado
         throw errors.UnprocessableEntity("A data inicial tem que ser maior que a data final.");
-    }
+    };
 
     //verificar se smallerDate e biggerDate foram passados
-    if (smallerDate && !biggerDate || !smallerDate && biggerDate ) {
+    if (smallerDate && !biggerDate || !smallerDate && biggerDate) {
+
+        // somente smallerDate não foi passado
         if (!smallerDate) {
             throw errors.BadRequest("Se deseja fazer o filtro usando um periodo de data informe a data menor")
-        } 
+        };
+
+        // somente biggerDate não foi passado
         if (!biggerDate) {
             throw errors.BadRequest("Se deseja fazer o filtro usando um periodo de data informe a data maior")
-        } 
-    }
+        };
+    };
 
-
-
-    //se tudo cer certo vou enviar os dados para o baco
+    //se tudo der certo vou enviar os dados para o banco
     const result = await flightsRepository.flightsGet(origin, destination, smallerDate, biggerDate);
     return result;
 }
 
-export const flightsServices = { flightsPost, flightsGet }
+export const flightsServices = { flightsPost, flightsGet };
